b89df218b0dc2e7ece97d855b30c71ab00489303
sven-zo <svenzo@outlook.com>
2018-08-19 20:17:36 +0200

    feat(base-styles): add styled components plugin

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,15 @@
 import React from 'react'
-import { Link } from 'gatsby'
 
 import Layout from '../components/layout'
+import styled from 'styled-components'
+
+const StyledComponentsTest = styled.p`
+  color: red;
+`
 
 const IndexPage = () => (
   <Layout>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <Link to="/page-2/">Go to page 2</Link>
+    <StyledComponentsTest>This text should be red.</StyledComponentsTest>
   </Layout>
 )
 
